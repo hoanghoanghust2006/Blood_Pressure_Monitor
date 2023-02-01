817ac8347f8ae5dc6f07801ffc2b731e850bd3d6
trung <trung>
2023-02-01 22:23:08 +0700

    Restrucure files

--- dev_code/dev_code.mk
+++ dev_code/dev_code.mk
@@ -1,15 +1,15 @@
 DEV_CODE_SOURCES = \
 dev_code/entry/src/entry.c \
-dev_code/driver_task/src/driver_task.c \
-dev_code/app_task/src/app_task.c \
-dev_code/led/src/led.c \
-dev_code/trace/src/trace.c \
+dev_code/task/driver_task/src/driver_task.c \
+dev_code/task/app_task/src/app_task.c \
+dev_code/component/led/src/led.c \
+dev_code/component/trace/src/trace.c
 
 
 DEV_CODE_INCLUDES = \
 -Idev_code/entry/inc \
--Idev_code/driver_task/inc \
--Idev_code/app_task/inc \
--Idev_code/led/inc \
--Idev_code/trace/inc \
--Idev_code/common/inc \
+-Idev_code/task/driver_task/inc \
+-Idev_code/task/app_task/inc \
+-Idev_code/component/led/inc \
+-Idev_code/component/trace/inc \
+-Idev_code/component/common/inc
