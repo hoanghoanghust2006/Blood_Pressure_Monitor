ca02f7a42537cd02480147e15003c35bc98ecb92
trang <trang>
2023-02-27 10:03:39 +0700

    Update AppTask

--- dev_code/component/stub/blood_pressure.c
+++ dev_code/component/stub/blood_pressure.c
@@ -1,6 +1,13 @@
 #include "blood_pressure.h"
 #include "trace.h"
 
+static tstValueMeasurement stValueMeasurement = {.u8Pressure  = 0,
+                                                 .u8Systolic  = 0,
+                                                 .u8Diastolic = 0,
+                                                 .u8HeartBeat = 0};
+
+static uint32_t u32Count = 0;
+
 void PRE_voRequestStartProcess(void)
 {
     trace("Request to start blood pressure\r\n");
@@ -9,11 +16,11 @@ void PRE_voRequestStartProcess(void)
 void PRE_voRequestCancelProcess(void)
 {
     trace("Request to stop blood pressure\r\n");
+    u32Count = 0;
 }
 
 tenNotifyResponse PRE_enGetStatusProcess(tstValueMeasurement *stGetResult)
 {
-    static uint32_t   u32Count      = 0;
     uint8_t           u8SimPressure = 0;
     tenNotifyResponse enRespond     = eNOT_NOTI_RES;
 
@@ -22,15 +29,18 @@ tenNotifyResponse PRE_enGetStatusProcess(tstValueMeasurement *stGetResult)
     {
         u8SimPressure = u32Count / 2;
         enRespond     = eBUSY;
+        stGetResult->u8Diastolic = 0;
     }
     else if ((u32Count > 300) && (u32Count <= 600))
     {
         u8SimPressure = (600 - u32Count) / 2;
         enRespond     = eBUSY;
+        stGetResult->u8Diastolic = 1;
     }
     else
     {
         enRespond = eSUCCESSFUL;
+        u32Count  = 0;
     }
 
     stGetResult->u8Pressure = u8SimPressure;
--- dev_code/dev_code.mk
+++ dev_code/dev_code.mk
@@ -5,6 +5,9 @@ dev_code/task/app_task/src/app_task.c \
 dev_code/component/led/src/led.c \
 dev_code/component/trace/src/trace.c \
 dev_code/component/button/src/button.c \
+dev_code/component/stub/display.c \
+dev_code/component/stub/blood_pressure.c \
+dev_code/component/storage/storage.c \
 dev_code/unit_test/test_button/src/test_button.c
 
 
@@ -16,4 +19,7 @@ DEV_CODE_INCLUDES = \
 -Idev_code/component/trace/inc \
 -Idev_code/component/common/inc \
 -Idev_code/component/button/inc \
+-Idev_code/component/display/inc \
+-Idev_code/component/blood_pressure/inc \
+-Idev_code/component/storage/inc \
 -Idev_code/unit_test/test_button/inc
