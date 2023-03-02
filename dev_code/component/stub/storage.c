dd5bbc07207ea0c9022b0ae67b7450e688f7e51e
Trung Nguyen <thanhtrung210502@gmail.com>
2023-03-02 17:04:02 +0700

    Treat warning as error

--- dev_code/component/stub/storage.c
+++ dev_code/component/stub/storage.c
@@ -8,12 +8,15 @@ void STO_voSaveRecord(tstStorage stNewRecord)
 
 uint8_t STO_u8GetNumOfRecords(void)
 {
+    return 0;
 }
 
 uint8_t STO_voGetRecords(tstStorage arrStorage[], uint8_t u8NumRequestRecord)
 {
+    return 0;
 }
 
 void STO_DeleteAllRecord(void)
 {
+    return;
 }
